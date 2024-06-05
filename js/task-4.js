c647164d083e41985f45ed7d400e32d1587bbff2
Alexandr Solovey <158730378+Solovey260885@users.noreply.github.com>
2024-06-05 12:39:53 +0300

    add task - 4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,17 @@
+const registerform = document.querySelector(".login-form");
+registerform.addEventListener("submit", handleSubmit);
 
+function handleSubmit(event) {
+  event.preventDefault();
+  const form = event.target;
+
+  const email = form.elements.email.value.trim();
+  const password = form.elements.password.value.trim();
+
+  if (email === "" || password === "") {
+    return console.log("All form fields must be filled in");
+  }
+
+  console.log(`Email: ${email}, Password: ${password}`);
+  form.reset();
+}
