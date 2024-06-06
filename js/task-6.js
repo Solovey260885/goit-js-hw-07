902fa7cfafd838829883c060d4b3857199436e88
Alexandr Solovey <158730378+Solovey260885@users.noreply.github.com>
2024-06-06 16:14:12 +0300

    start task-6

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,19 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const input = document.querySelector("input");
+
+const btnCreate = document.querySelector("#controls").children[1];
+
+const btnDestroy = document.querySelector("#controls").lastElementChild;
+const divBoxes = document.querySelector("#boxes");
+
+btnCreate.addEventListener("click", createBoxes);
+btnDestroy.addEventListener("click", clickDestroy);
+
+function createBoxes(amount) {
+  let div = `<div id="boxes1"></div>`;
+  divBoxes.insertAdjacentHTML("afterbegin", div);
+}
+function clickDestroy() {}
