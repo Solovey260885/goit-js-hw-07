59a2d500e66de3f3355ed1d2a10a72a9e5170ce3
Alexandr Solovey <158730378+Solovey260885@users.noreply.github.com>
2024-06-06 15:29:22 +0300

    add task-5

--- js/task-5.js
+++ js/task-5.js
@@ -1,3 +1,13 @@
+const body = document.querySelector("body");
+const btn = document.querySelector(".change-color");
+const nameBgColorBody = document.querySelector(".color");
+
+function changeBgColorOnClick() {
+  body.style.background = getRandomHexColor();
+  nameBgColorBody.textContent = getRandomHexColor();
+}
+btn.addEventListener("click", changeBgColorOnClick);
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
