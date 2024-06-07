d6d769b53e926587f269deb1867e585740511966
Alexandr Solovey <158730378+Solovey260885@users.noreply.github.com>
2024-06-07 22:41:04 +0300

    first finish hw 7

--- js/task-6.js
+++ js/task-6.js
@@ -12,10 +12,32 @@ const btnDestroy = document.querySelector("#controls").lastElementChild;
 const divBoxes = document.querySelector("#boxes");
 
 btnCreate.addEventListener("click", createBoxes);
-btnDestroy.addEventListener("click", clickDestroy);
+btnDestroy.addEventListener("click", destroyBoxes);
 
-function createBoxes(amount) {
-  let div = `<div id="boxes1"></div>`;
-  divBoxes.insertAdjacentHTML("afterbegin", div);
+function createBoxes() {
+  divBoxes.innerHTML = "";
+
+  let divsHTML = "";
+  const startWidth = 30 - 10;
+  const startHeight = 30 - 10;
+
+  let amount = 0;
+  amount = input.value;
+  let width = 0;
+  let height = 0;
+
+  if (amount >= 1 && amount <= 100) {
+    for (let i = 1; i <= amount; i++) {
+      width = startWidth + 10 * i;
+      height = startHeight + 10 * i;
+      divsHTML = `<div
+      style="width: ${width}px; height: ${height}px; background-color: ${getRandomHexColor()}"
+      </div>`;
+      divBoxes.insertAdjacentHTML("afterbegin", divsHTML);
+    }
+  }
+  input.value = "";
 }
-function clickDestroy() {}
+function destroyBoxes() {
+  divBoxes.innerHTML = "";
+}
